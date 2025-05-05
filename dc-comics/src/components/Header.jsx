f6bd146e61bfdcedcdbe826c76ba48d7e7beab9f
mannuccimattia <mannuccimattia@gmail.com>
2025-05-05 15:37:08 +0200

    [UPD] Done header - raw style

--- dc-comics/src/components/Header.jsx
+++ dc-comics/src/components/Header.jsx
@@ -1,8 +1,26 @@
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header>
+      <div className="container">
+        <figure>
+          <img src="../src/assets/img/dc-logo.png" alt="logo DC comics" />
+        </figure>
+        <nav>
+          <ul>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#" className="active">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">Tv</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Collectibles</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">Fans</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Shop</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   )
 }
 
