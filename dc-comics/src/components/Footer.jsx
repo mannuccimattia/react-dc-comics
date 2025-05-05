31527ba52ea15e6eeb352809ca9f8cee8d0f7da6
mannuccimattia <mannuccimattia@gmail.com>
2025-05-05 17:03:53 +0200

    [UPD] Done top footer - raw style

--- dc-comics/src/components/Footer.jsx
+++ dc-comics/src/components/Footer.jsx
@@ -1,8 +1,67 @@
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer>
+      <section className="top-footer">
+        <div className="container">
+          <div className="content">
+            <div className="footer-col left">
+              <div className="footer-subcol">
+                <h3>DC COMICS</h3>
+                <ul>
+                  <li><a href="#">Characters</a></li>
+                  <li><a href="#">Comics</a></li>
+                  <li><a href="#">Movies</a></li>
+                  <li><a href="#">Tv</a></li>
+                  <li><a href="#">Games</a></li>
+                  <li><a href="#">Collectibles</a></li>
+                  <li><a href="#">Videos</a></li>
+                  <li><a href="#">Fans</a></li>
+                  <li><a href="#">News</a></li>
+                  <li><a href="#">Shop</a></li>
+                </ul>
+                <h3>SHOP</h3>
+                <ul>
+                  <li><a href="#">Shop DC</a></li>
+                  <li><a href="#">Shop DC Collectibles</a></li>
+                </ul>
+              </div>
+              <div className="footer-subcol">
+                <h3>DC</h3>
+                <ul>
+                  <li><a href="#">Terms of Use</a></li>
+                  <li><a href="#">Privacy policy &#40;New&#41;</a></li>
+                  <li><a href="#">Ad Choices</a></li>
+                  <li><a href="#">Advertising</a></li>
+                  <li><a href="#">Jobs</a></li>
+                  <li><a href="#">Subscriptions</a></li>
+                  <li><a href="#">Talent Workshops</a></li>
+                  <li><a href="#">CPSC Certificates</a></li>
+                  <li><a href="#">Ratings</a></li>
+                  <li><a href="#">Shop Help</a></li>
+                  <li><a href="#">Contact Us</a></li>
+                </ul>
+              </div>
+              <div className="footer-subcol">
+                <h3>SITES</h3>
+                <ul>
+                  <li><a href="#">DC</a></li>
+                  <li><a href="#">MAD Magazine</a></li>
+                  <li><a href="#">DC Kids</a></li>
+                  <li><a href="#">DC Universe</a></li>
+                  <li><a href="#">DC Power Visa</a></li>
+                </ul>
+              </div>
+            </div>
+            <div className="footer-col right">
+              <figure></figure>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="bottom-footer">
+        bottom footer
+      </section>
+    </footer>
   )
 }
 
